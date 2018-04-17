b1c2e1547777625231188c7c3c2c95b416b19e9c
justinemini <justinejur@gmail.com>
2018-04-17 19:53:06 +0300

    sprintf

---
+++ PrasidedaProgramavimas/string-sprintf.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = "%a yra %a studentas<br>"; //turi istatyt i sablona reiksmes:
+
+echo sprintf($a, 'Jonas', 'KTU');
+echo sprintf($a, 'Petras', 'VDU');
+echo sprintf($a, 'Antanas', 'Codeacademy');
+
+for ($i = 0; $i<20; $i++){
+//echo $i . '<br>';
+//echo sprintf("%02d", $i) . '<br>';
+echo sprintf("%03d", $i) . '<br>';
+}
