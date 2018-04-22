136e1a81b8fe53a01efe252f6c5f797e6c9fc46a
justinemini <justinejur@gmail.com>
2018-04-22 19:10:49 +0300

    kontrolinis3

--- NamuDarbai/kontroliaiDarbinis/kontrolinisnr3.php
+++ NamuDarbai/kontroliaiDarbinis/kontrolinisnr3.php
@@ -3,7 +3,7 @@
 Sukurti klasę, kuri turi savybę - skaičių sąrašas. Sukurti metodą, kuris sugeneruotų atsitiktinį skaičių intervale nuo 1 iki 100 ir patalpintų į sąrašą, o po to surūšiuotų sąrašą. Sukurti metodą, kuris gražiną sugeneruotų skaičių aritmetitinį vidurkį. Sukurti metodą, kuris išveda masyvo elementus tekstu, kur masyvo elementai atskirti kableliais. Pademonstruoti veikimą.
  */
 
-class savybe
+class MasyvoRedagavimas
 {
     public $skaiciusarasas = [];
 
@@ -12,15 +12,33 @@ class savybe
         $this->skaiciusarasas[] = rand(0, 100);
         sort($this->skaiciusarasas);
     }
-    function vidurkis(){
+    function vidurkis()
+    {
         $suma = 0;
         foreach ($this->skaiciusarasas as $atsitiktinis){
             $suma +=$atsitiktinis;
         }
         return $suma / count($this->skaiciusarasas);
     }
+    function gautiTeksta(){
+        return implode(', ', $this->skaiciusarasas);
+    }
 }
 
+$sarasas = new MasyvoRedagavimas();
+$sarasas->atsitiktinis();
+$sarasas->atsitiktinis();
+$sarasas->atsitiktinis();
+$sarasas->atsitiktinis();
+$sarasas->atsitiktinis();
+$sarasas->atsitiktinis();
+$sarasas->atsitiktinis();
+echo '<br>';
+echo $sarasas->vidurkis();
+echo '<br>';
+echo $sarasas->gautiTeksta();
+echo '<br>';
+var_dump($sarasas);
 
 
 
